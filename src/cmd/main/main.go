f89ff1a9a35cb1b22a87cdc1b1cf3ebd52877b79
ganadipa <nyomanganadipa@gmail.com>
2024-05-28 01:45:16 +0700

    Nit: golang csv read

--- src/cmd/main/main.go
+++ src/cmd/main/main.go
@@ -1,8 +1,54 @@
 // src/cmd/main/main.go
 package main
 
-import "fmt"
+import (
+	"encoding/csv"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+
+	// Get transition matrix from "../data/dago-matrix.csv"
+
+	var matrix [][]float64
+	matrix = getMatrix("../data/dago-matrix.csv")
+
+	for _, row := range matrix {
+		fmt.Println(row)
+	}
+}
+
+func getMatrix(path string) [][]float64 {
+	// Open the CSV file
+	file, err := os.Open(path)
+	if err != nil {
+		log.Fatalf("failed to open file: %s", err)
+	}
+	defer file.Close()
+
+	// Create a new CSV reader
+	reader := csv.NewReader(file)
+
+	// Read all the records from the CSV file
+	records, err := reader.ReadAll()
+	if err != nil {
+		log.Fatalf("failed to read file: %s", err)
+	}
+
+	// Convert the records into a 2D slice of integers
+	matrix := make([][]float64, len(records))
+	for i, record := range records {
+		matrix[i] = make([]float64, len(record))
+		for j, value := range record {
+			matrix[i][j], err = strconv.ParseFloat(value, 64)
+			if err != nil {
+				log.Fatalf("failed to convert string to int: %s", err)
+			}
+		}
+	}
+
+	return matrix
 }
